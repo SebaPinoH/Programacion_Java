56a826cd52c329a8fb45dbf04e9d2630415ee5ec
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-11-06 16:10:25 -0300

    Create Leer_Nombre.java

---
+++ Ejercicios_Basicos/Leer_Nombre.java
@@ -0,0 +1,15 @@
+//Introduce nombre por pantalla, y lo muestra en pantalla
+
+import java.util.Scanner;
+
+public class Leer_Nombre {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Introduce un nombre: ");
+        cadena = sc.nextLine();
+        System.out.println("Buenos Días " + cadena);                                               
+    }
+}
