9832a74d9dd6278c7ff6bdf05b940e3c7f4e9030
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-11-06 18:35:24 -0300

    Create NumeroXDoble.java

---
+++ Ejercicios_Basicos/NumeroXDoble.java
@@ -0,0 +1,24 @@
+//Ingresar numero por pantalla y este muestra el doble y el triple del numero
+
+import java.util.Scanner;
+
+public class NumeroXDoble {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Introduce un número entero:");
+        numero = sc.nextInt();
+        System.out.println("Número introducido: " + numero);
+        System.out.println("Doble de " + numero + " -> "+ 2*numero);
+        System.out.println("Triple de " + numero + " -> "+ 3*numero);                              
+       
+    }
+
+}
+
+
+
+
