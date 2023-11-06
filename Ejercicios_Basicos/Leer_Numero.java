7c8d26d4bad4491a64be9c7730a9d2493e01e777
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-11-06 16:10:27 -0300

    Create Leer_Numero.java

---
+++ Ejercicios_Basicos/Leer_Numero.java
@@ -0,0 +1,25 @@
+//Introduce numero por teclado y los muestra en pantalla
+
+import java.util.Scanner;
+
+ public class Leer_Numero {
+
+    public static void main(String[] args){
+
+        //declaración de variables
+        int n1, n2;
+        Scanner sc = new Scanner(System.in);
+
+        //leer el primer número
+        System.out.println("Introduce el primero número: ");
+        n1 = sc.nextInt();      //lee un entero por teclado
+
+        //leer el segundo número
+        System.out.println("Introduce el segundo número: ");
+        n2 = sc.nextInt();      //lee un entero por teclado
+        
+        //mostrar resultado
+        System.out.println("Ha introducido los números: " + n1 + " y " + n2);                      
+
+    }
+}
